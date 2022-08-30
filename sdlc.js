ff4a869d239295d934e7b8f8030e5a99bedfb1e7
Darren Wott <darren.wott@gmail.com>
2022-08-29 17:23:13 -0700

    solved sdlc problem

--- sdlc.js
+++ sdlc.js
@@ -12,10 +12,22 @@
 // Testing & Integration
 // Planning
 
+// Planning
+// Analyst of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle 
+    and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analystOfRequirements = "This is the second step. This stage of the SDLC is getting input from all stakeholders, iuncluding customers, sales peopole, industry experts, and programmers. Lear the stengths and weaknesses of the current system with improvement as the goal."
+const design = "This is the third step. This phase of the SDLC starts by turning the software specifications into a design plan called the design specification."
+const implementation = "This is the fourth step. At this stage, the actual development starts. It's important that every developer sticks to the agreed blueprint"
+const testingAndIntegration = "This is the fifth step. In the stage, we test for defects and deficiencies. We fix those issues until the product meets the original specifications."
+const maintain = "This is the sixth step. The plan almost never turns out perfect when it meets reality. Further, as conditions in the real world change, we need to update and advance the software to match."
